80a00ae83d392a38de0c6c77f3ec5db351b63846
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-05 12:19:19 -0300

    Atualizando pessoa com PUT

--- src/main/java/com/modulo/projetoapi/resource/PessoaResource.java
+++ src/main/java/com/modulo/projetoapi/resource/PessoaResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.modulo.projetoapi.event.RecursoCriadoEvent;
 import com.modulo.projetoapi.model.Pessoa;
 import com.modulo.projetoapi.repository.PessoaRepository;
+import com.modulo.projetoapi.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -31,6 +33,9 @@ public class PessoaResource {
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
+	
+	@Autowired
+	private PessoaService pessoaService;
 
 	@GetMapping
 	public List<Pessoa> findAll() {
@@ -55,5 +60,11 @@ public class PessoaResource {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long codigo) {
 		pessoaRepository.delete(codigo);
+	}
+	
+	@PutMapping(value = "/{codigo}")
+	public ResponseEntity<Pessoa> update(@PathVariable Long codigo,@Valid @RequestBody Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaService.update(codigo, pessoa);
+		return ResponseEntity.ok(pessoaSalva);
 	}
 }
---
+++ src/main/java/com/modulo/projetoapi/service/PessoaService.java
@@ -0,0 +1,25 @@
+package com.modulo.projetoapi.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.modulo.projetoapi.model.Pessoa;
+import com.modulo.projetoapi.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Pessoa update(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaRepository.findOne(codigo);
+		if (pessoaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo"); // copia as propriedades para o pessoaSalva(1° parâmetro vc passa o Objeto que quer copiar,2° parâmetro o Objeto que vai receber e 3° parâmetro as propriedades que será ignorada.)
+		return pessoaRepository.save(pessoaSalva);
+	}
+}
