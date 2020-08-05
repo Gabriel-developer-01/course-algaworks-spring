8a95651ecad296fa35db35b705336bcb9d088b70
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-05 12:13:15 -0300

    Removendo pessoa com DELETE

--- src/main/java/com/modulo/projetoapi/resource/PessoaResource.java
+++ src/main/java/com/modulo/projetoapi/resource/PessoaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.modulo.projetoapi.event.RecursoCriadoEvent;
@@ -47,5 +49,11 @@ public class PessoaResource {
 
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalvar.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalvar);
+	}
+	
+	@DeleteMapping(value = "/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long codigo) {
+		pessoaRepository.delete(codigo);
 	}
 }
