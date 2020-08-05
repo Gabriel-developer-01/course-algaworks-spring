e8ad8193e1e0cd556b89013045f3aff4de6a64e1
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-05 15:15:25 -0300

    Cadastrando lançamentos

--- src/main/java/com/modulo/projetoapi/resource/LancamentoResource.java
+++ src/main/java/com/modulo/projetoapi/resource/LancamentoResource.java
@@ -2,13 +2,21 @@ package com.modulo.projetoapi.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.modulo.projetoapi.event.RecursoCriadoEvent;
 import com.modulo.projetoapi.model.Lancamento;
 import com.modulo.projetoapi.repository.LancamentoRepository;
 
@@ -18,6 +26,9 @@ public class LancamentoResource {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Lancamento> findAll(){
@@ -29,5 +40,11 @@ public class LancamentoResource {
 		Lancamento findById= lancamentoRepository.findOne(codigo);
 		return !(findById == null) ? ResponseEntity.ok().body(findById) : ResponseEntity.notFound().build();
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> insert(@Valid @RequestBody Lancamento lancamento,HttpServletResponse response){
+		Lancamento lancamentoSave = lancamentoRepository.save(lancamento);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSave.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSave);
+	}
 }
