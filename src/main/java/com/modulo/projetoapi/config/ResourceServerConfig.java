775de9f93f46075bf8c768a2f973221d3fb8e9b5
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-07 17:01:00 -0300

    resolvendo detalhes

--- src/main/java/com/modulo/projetoapi/config/ResourceServerConfig.java
+++ src/main/java/com/modulo/projetoapi/config/ResourceServerConfig.java
@@ -33,8 +33,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/categorias")
-		.permitAll().anyRequest().authenticated()
+		.antMatchers("/categorias").permitAll()
+		.anyRequest().authenticated()
 		.and()
 		.sessionManagement()
 		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
