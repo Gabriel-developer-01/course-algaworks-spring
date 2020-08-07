0155cf4606a76184e92c1847127b86ad5666b3f7
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-07 16:30:30 -0300

    Movendo o usuário para o banco de dados - SEGURANÇA NA API

--- src/main/java/com/modulo/projetoapi/config/ResourceServerConfig.java
+++ src/main/java/com/modulo/projetoapi/config/ResourceServerConfig.java
@@ -1,44 +1,55 @@
 package com.modulo.projetoapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-
 
 //Fornece os recursos após confirmação de autenticação
 @Configuration
 @EnableWebSecurity
 @EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("admin")
-		.password("admin")
-		.roles("ROLE");
-	}
+	private UserDetailsService userDetailsService;
+
 	
-	//Configuração de autorização das requisições
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	// Configuração de autorização das requisições
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/categorias").permitAll()
-		.anyRequest().authenticated()
+		.antMatchers("/categorias")
+		.permitAll().anyRequest().authenticated()
 		.and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 		.csrf().disable();
 	}
-	
-	//Deixando como stateless sem armazenar estado de sessão no servidor
+
+	// Deixando como stateless sem armazenar estado de sessão no servidor
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.stateless(true);
+	}
+
+	// método para conseguir ler a senha encodada
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
---
+++ src/main/java/com/modulo/projetoapi/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.modulo.projetoapi.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.modulo.projetoapi.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findByEmail(String email);
+	
+}
