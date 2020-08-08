9c5bda2ea5c539608a2835a8acb88f4f8ec6e832
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-08 15:49:50 -0300

    Profiles do Spring

--- src/main/java/com/modulo/projetoapi/ProjetoApiApplication.java
+++ src/main/java/com/modulo/projetoapi/ProjetoApiApplication.java
@@ -2,8 +2,12 @@ package com.modulo.projetoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.modulo.projetoapi.config.property.AlgamoneyApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class ProjetoApiApplication {
 
 	public static void main(String[] args) {
