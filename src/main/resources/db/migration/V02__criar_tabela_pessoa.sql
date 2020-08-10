61e458e683e2d5136b0666b4acbec618b39c42b6
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-08-10 14:54:13 -0300

    Ajuste nos dados - reorganizando o último commit

--- src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
+++ src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
@@ -11,13 +11,23 @@ cidade VARCHAR(50),
 estado VARCHAR(2)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) 
-VALUES ('Chuck Norris', true, 'Brooklin street win', '07', null, 'Brooklin','05.555-777','Nova York','ex');
-INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) 
-VALUES ('Dercy Gonçalves', false, 'Rua da zueira s/n conjunto z lote', '01', 'Zueiropolis', 'Rio de janeiro','05.555-777','Rio de Janeiro','rj');
-INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) 
-VALUES ('Marcos Sousa', true, 'SQS 211 Bloco B apt', '301', null, 'Asa Sul','05.555-777','Brasilia','df');
-INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) 
-VALUES ('Zeze Polessa', true, 'Avenida das palmeiras conunto w casa', '01', 'Bonfinopolis', 'Bonfinopolis','05.555-777','Rio de Janeiro','rj');
-INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) 
-VALUES ('Ayrton Senna', true, 'Avenida dos campeões Washington Luiz casa', '12', null, 'Interlagos','72.000-710','São Paulo','sp');
+INSERT INTO pessoa (nome, ativo, logradouro, numero , complemento, bairro , cep, cidade, estado )
+ VALUES ('Silvia',true,'Rua Caetano Moura','25','Em frente casa','Federação','38.400-121','Salvador','BA');
+ INSERT INTO pessoa (nome, ativo, logradouro, numero , complemento, bairro , cep, cidade, estado )
+ VALUES ('Maria',true,'Rua José Ramos','10','ao lado da igreja','Vasco da Gama','11.400-121','Salvador','BA');
+ INSERT INTO pessoa (nome, ativo,logradouro, numero , complemento, bairro , cep, cidade, estado )
+ VALUES ('Marcia',true,'Rua Paraná','22','Em frente lachonente','Trancredo Neves','54.212-121','Salvador','BA');
+ INSERT INTO pessoa (nome, ativo,logradouro, numero , complemento, bairro , cep, cidade, estado )
+ VALUES ('Silvana',true,'Rua Alagoinhas','43','Em frente curso','Rio vermelho','38.400-12','Salvador','BA');
+ INSERT INTO pessoa (nome, ativo,logradouro, numero , complemento, bairro , cep, cidade, estado )
+ VALUES ('Matilde',false,'Alto de Ondina','50','Em frente mata','ondina','56.400-121','Salvador','BA');
+ INSERT INTO pessoa (nome, ativo,logradouro, numero , complemento, bairro , cep, cidade, estado )
+ VALUES ('carla',false,'Rua Silveira Martins','55','a direita do material de construção','Pernambues','77.400-121','Salvador','BA');
+ INSERT INTO pessoa (nome, ativo,logradouro, numero , complemento, bairro , cep, cidade, estado )
+ VALUES ('vitor',true,'Rua Oito de Dezembro','66','Em frente policia','barra','12.400-121','Salvador','BA');
+ INSERT INTO pessoa (nome, ativo,logradouro, numero , complemento, bairro , cep, cidade, estado )
+ VALUES ('Ramos',true,'Rua Pedro Gama','75','Em frente hospital','Federação','38.400-00','Salvador','BA');
+ INSERT INTO pessoa (nome, ativo,logradouro, numero , complemento, bairro , cep, cidade, estado )
+ VALUES ('Soltinho',true,'Rua Fernando Falcão','67','Em frente medeiros','Sao Cristovão','78.400-325','Salvador','BA');
+ INSERT INTO pessoa (nome, ativo,logradouro, numero , complemento, bairro , cep, cidade, estado )
+ VALUES ('Katia',true,'Rua Patativas','27','Em frente ao restaurante','Imbuí','86.400-235','Salvador','BA');
